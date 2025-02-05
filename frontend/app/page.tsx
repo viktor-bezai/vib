8dabf829f1fd46ba5bcf259299145f8150b335d6
Viktor Bezai <viktorbezai@gmail.com>
2025-02-05 13:14:00 -0500

    83 - need to adapt video page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,18 +18,37 @@ export default function HomePage() {
       }}
     >
       {/* Main Heading */}
-      <Typography variant="h3" gutterBottom sx={{ fontWeight: "bold", color: "var(--primary-color)" }}>
-        {"Welcome to Learn English App!"}
+      <Typography
+          variant="h3"
+          gutterBottom
+          sx={{
+            fontWeight: "bold",
+            color: "var(--primary-color)",
+            fontSize: { xs: "1.8rem", sm: "2.2rem", md: "2.8rem" }, // Responsive font size
+          }}
+        >
+          {"Welcome to my personal Web App!"}
       </Typography>
 
       {/* Subheading */}
-      <Typography variant="h5" gutterBottom >
-        Enjoy learning English by <Link href={"/video"}>watching videos</Link> with the words of your choice.
-      </Typography>
-
-      {/* Message about LinkedIn */}
-      <Typography variant="h6" gutterBottom sx={{ marginTop: "1rem" }}>
-        {"If you'd like to chat, feel free to connect with me on LinkedIn."}
+      <Typography
+      variant="h5"
+      gutterBottom
+      sx={{
+            fontSize: { xs: "1.2rem", sm: "1.5rem", md: "2rem" },
+          }}
+      >
+        {"Hi, I'm Viktor Bezai. "} <br />
+          {"Learning English by "}
+          <Link href={"/video"}>
+            watching videos
+          </Link>{" "}
+          {"is in development and will be available soon."} <br />
+          {"You can download my resume on the "}
+          <Link href={"/about"}>
+            About page
+          </Link>
+          {" or connect with me on LinkedIn."}
       </Typography>
 
       {/* LinkedIn Button */}
